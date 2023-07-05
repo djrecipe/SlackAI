fda2a51f3fdf7b2a10b52596604e77d6d97d22d9
JD Pierce <brkfstmnchr@gmail.com>
2023-07-05 20:03:56 +0700

    working proof of concept
    working proof of concept

---
+++ SlackAI/SlackAI/Program.cs
@@ -0,0 +1,26 @@
+﻿using SlackAI;
+using SlackNet;
+using SlackNet.WebApi;
+using SlackNet.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SlackNet.Blocks;
+using SlackNet.Events;
+using SlackNet.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSlackNet(c => c
+    // Configure the tokens used to authenticate with Slack
+    .UseApiToken("") // This gets used by the API client
+    .UseAppLevelToken(
+        "") // This gets used by the socket mode client
+
+    .RegisterSlashCommandHandler<SlashCommandHandler>("/ai")
+);
+var services = serviceCollection.BuildServiceProvider();
+
+var client = services.SlackServices().GetSocketModeClient();
+await client.Connect();
+
+Console.WriteLine("Connected. Press any key to exit...");
+await Task.Run(Console.ReadKey);
---
+++ SlackAI/SlackAI/SlashCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using LLama;
+using LLama.Common;
+using LLama.OldVersion;
+using Newtonsoft.Json.Linq;
+using SlackNet;
+using SlackNet.Events;
+using SlackNet.Interaction;
+using SlackNet.Interaction.Experimental;
+using SlackNet.WebApi;
+using LLamaModel = LLama.OldVersion.LLamaModel;
+
+namespace SlackAI;
+
+/// <summary>
+/// A slash command handler that just echos back whatever you sent it 
+/// </summary>
+class SlashCommandHandler : ISlashCommandHandler
+{
+    private ChatSession session;
+    private readonly ISlackApiClient _slack;
+    public SlashCommandHandler(ISlackApiClient slack)
+    {
+        _slack = slack;
+        var ex = new InteractiveExecutor(
+            new LLama.LLamaModel(new ModelParams("C:\\Downloads\\wizardLM-7B.ggmlv3.q4_0.bin")));
+        session = new ChatSession(ex);
+    }
+    public async Task<SlashCommandResponse> Handle(SlashCommand command)
+    {
+        Console.WriteLine($"{command.UserName} used the {"/ai"} slash command in the {command.ChannelName} channel");
+        Task.Run(() => AskAI(command.Text, command.ChannelName));
+        return new SlashCommandResponse
+        {
+            Message = new Message
+            {
+                Text = "Please wait a moment..."
+            }
+        };
+    }
+
+    public void AskAI(string prompt, string channel)
+    {
+        Console.WriteLine($"> {prompt}");
+        string result = "";
+        var response = session.Chat(prompt,
+            new InferenceParams() {Temperature = 0.6f, MaxTokens = 500, FrequencyPenalty = 1.0f, PenalizeNL = false, AntiPrompts = new List<string> { "User:" } });
+        foreach (var item in response)
+        {
+            Console.Write(item);
+            result += $"{item}";
+        }
+        result = result.Remove(result.Length - 5, 5);
+        _slack.Chat.PostMessage(new Message()
+            {
+                Channel = channel, 
+                Text = $">{prompt}\n{result}"
+
+            }
+        );
+    }
+}
